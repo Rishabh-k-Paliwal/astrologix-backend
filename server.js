b1013d468cef187efd04199b0fc08ddcb1acd245
Rishabh-k-Paliwal <rishabh.kp.04@gmail.com>
2025-09-06 12:38:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ app.use(helmet({
 app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:3000",
   credentials: true,
-  optionsSuccessStatus: 200
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Rate limiting
